315f651023f6a0a1a59419bdd802e99f06e62269
omb05308-cell <omb05308@gmail.com>
2026-09-21 11:23:44 +0530

    Add student roll number display program

---
+++ student_display.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int roll1, roll2, roll3, roll4, roll5;
+	
+	cout<<"Enter the first roll no of the studdent: ";
+	cin>>roll1;
+	cout<<"Enter the second roll no of the student: ";
+	cin>>roll2;
+	cout<<"Enter the third roll no of the student: ";
+	cin>>roll3;
+	cout<<"Enter the fourth roll no of the student: ";
+	cin>>roll4;
+	cout<<"Enter the fifth roll no of the student: ";
+	cin>>roll5;
+	
+	//display
+	cout<<"\n------------Roll No --------------------\n";
+	
+	cout<<roll1<<endl;
+	cout<<roll2<<endl;
+	cout<<roll3<<endl;
+	cout<<roll4<<endl;
+	cout<<roll5<<endl;
+	
+	return 0;
+}
