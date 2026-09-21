d682ad29ddbd4a85a2ac7d657276b9d252985202
omb05308-cell <omb05308@gmail.com>
2026-09-21 13:11:41 +0530

    Create student_ser.cpp

---
+++ student_ser.cpp
@@ -0,0 +1,27 @@
+
+using namespace std;
+int main()
+{
+	int rollno[5];
+	int searchrollno;
+	
+	cout<<"Enter 5 roll number:\n";
+
+	for (int i=0; i<5; i++)
+	{
+	  cin>> rollno[i];
+	}
+
+	cout<<"\nEnter rollno to search: ";
+	cin>> searchrollno;
+
+	for (int i=0; i<5; i++)
+	{
+	   if (rollno[i] == searchrollno)
+	   { 
+		cout<<"Student Found!";
+		return 0;
+	   }
+	}
+	cout<<"Student not Found!";
+	return 0;
